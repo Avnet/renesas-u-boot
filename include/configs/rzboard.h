5b4b7a3ced8c7e882f24c2fa49bf5ce6f12447a4
Nick <nick.cheng@avnet.com>
2022-02-14 03:52:22 +0000

    uEnv:rzboard:Add uEnv.txt support
         add uEnv.txt support, which will load additional environment variables flexibly
    
    Signed-off-by: Nick <nick.cheng@avnet.com>

--- include/configs/rzboard.h
+++ include/configs/rzboard.h
@@ -57,9 +57,33 @@
 /* The HF/QSPI layout permits up to 1 MiB large bootloader blob */
 #define CONFIG_BOARD_SIZE_LIMIT		1048576
 
+/* support uEnv.txt to pass environment variables to the kernel */
+#ifndef CONFIG_CMD_IMPORTENV
+#define CONFIG_CMD_IMPORTENV
+#endif
+
+#define DEFAULT_MMC_UENV_ARGS \
+	"bootenvfile=uEnv.txt\0" \
+	"importbootenv=echo Importing environment from mmc${mmcdev} ...; " \
+		"env import -t ${image_loadaddr} ${filesize}\0" \
+	"loadbootenv=fatload mmc ${mmcdev} ${image_loadaddr} ${bootenvfile}\0" \
+	"envboot=mmc dev ${mmcdev}; " \
+		"if mmc rescan; then " \
+			"echo SD/MMC found on device ${mmcdev};" \
+			"if run loadbootenv; then " \
+				"echo Loaded env from ${bootenvfile};" \
+				"run importbootenv;" \
+			"fi;" \
+		"fi;\0"
+
+#define MMC_BOOT_UENV  "run envboot;"
+#define DEFAULT_MMC_RZV2L_ARGS  DEFAULT_MMC_UENV_ARGS
+
+
 /* ENV setting */
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
+	DEFAULT_MMC_RZV2L_ARGS \
 	"fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0" \
 	"image=Image \0" \
 	"mmcdev="__stringify(CONFIG_SYS_MMC_ENV_DEV)"\0" \
@@ -69,11 +93,13 @@
 	"bootm_size=0x10000000 \0" \
 	"mmcbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk0p2 \0" \
 	"loadimage=fatload mmc ${mmcdev}:${mmcpart} ${image_loadaddr} ${image}\0" \
-	"loadfdt=fatload mmc ${mmcdev}:${mmcpart} ${fdt_loadaddr} ${fdtfile}\0" \
+	"loadfdt=echo loading ${fdtfile};fatload mmc ${mmcdev}:${mmcpart} ${fdt_loadaddr} ${fdtfile}\0" \
 	"mmcload=mmc dev ${mmcdev};run loadfdt;run loadimage;run mmcbootargs \0" \
 	"bootimage=run mmcload; booti 0x48080000 - 0x48000000 \0"
 
-#define CONFIG_BOOTCOMMAND	"env default -a;run bootimage"
+#define CONFIG_BOOTCOMMAND	\
+	MMC_BOOT_UENV \
+	"run bootimage"
 
 /* For board */
 /* Ethernet RAVB */
