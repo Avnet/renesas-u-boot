47fd69c98cf6db9edebe86169ec965acd66b2fea
Nick <nick.cheng@avnet.com>
2022-08-17 11:41:20 +0800

    bootup:rzboard: Upgrade Avnet RzBoard to v2021.10
    	Ported the code for rzboard from branch rzboard_v2l_v2020.10
    
    Signed-off-by: Nick <nick.cheng@avnet.com>

---
+++ board/renesas/rzboard/rzboard.c
@@ -0,0 +1,150 @@
+#include <common.h>
+#include <cpu_func.h>
+#include <image.h>
+#include <init.h>
+#include <malloc.h>
+#include <netdev.h>
+#include <dm.h>
+#include <dm/platform_data/serial_sh.h>
+#include <asm/processor.h>
+#include <asm/mach-types.h>
+#include <asm/io.h>
+#include <linux/bitops.h>
+#include <linux/errno.h>
+#include <linux/delay.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/gpio.h>
+#include <asm/arch/gpio.h>
+#include <asm/arch/rmobile.h>
+#include <asm/arch/rcar-mstp.h>
+#include <asm/arch/sh_sdhi.h>
+#include <i2c.h>
+#include <mmc.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+#define PFC_BASE	0x11030000
+
+#define ETH_CH0		(PFC_BASE + 0x300c)
+#define ETH_CH1		(PFC_BASE + 0x3010)
+#define I2C_CH1		(PFC_BASE + 0x1870)
+#define ETH_PVDD_3300	0x00
+#define ETH_PVDD_1800	0x01
+#define ETH_PVDD_2500	0x02
+#define ETH_MII_RGMII	(PFC_BASE + 0x3018)
+
+/* CPG */
+#define CPG_BASE					0x11010000
+#define CPG_CLKON_BASE				(CPG_BASE + 0x500)
+#define CPG_RESET_BASE				(CPG_BASE + 0x800)
+#define CPG_RESET_ETH				(CPG_RESET_BASE + 0x7C)
+#define CPG_RESET_I2C				(CPG_RESET_BASE + 0x80)
+#define CPG_PL2_SDHI_DSEL			(CPG_BASE + 0x218)
+#define CPG_CLK_STATUS				(CPG_BASE + 0x280)
+
+/* PFC */
+#define PFC_P37						(PFC_BASE + 0x037)
+#define PFC_PM37					(PFC_BASE + 0x16E)
+#define PFC_PMC37					(PFC_BASE + 0x237)
+
+void s_init(void)
+{
+	/* SD1 power control: P39_1 = 0; P39_2 = 1; */
+	*(volatile u32 *)(PFC_PMC37) &= 0xFFFFFFF9; /* Port func mode 0b00 */
+	*(volatile u32 *)(PFC_PM37) = (*(volatile u32 *)(PFC_PM37) & 0xFFFFFFC3) | 0x28; /* Port output mode 0b1010 */
+	*(volatile u32 *)(PFC_P37) = (*(volatile u32 *)(PFC_P37) & 0xFFFFFFF9) | 0x6;	/* Port 39[2:1] output value 0b11*/
+	
+	/* can go in board_eht_init() once enabled */
+	*(volatile u32 *)(ETH_CH0) = (*(volatile u32 *)(ETH_CH0) & 0xFFFFFFFC) | ETH_PVDD_1800;
+	/* Enable RGMII for both ETH{0,1} */
+	*(volatile u32 *)(ETH_MII_RGMII) = (*(volatile u32 *)(ETH_MII_RGMII) & 0xFFFFFFFC);
+	/* ETH CLK */
+	*(volatile u32 *)(CPG_RESET_ETH) = 0x30003;
+	/* I2C CLK */
+	*(volatile u32 *)(CPG_RESET_I2C) = 0xF000F;
+	/* I2C pin non GPIO enable */
+	*(volatile u32 *)(I2C_CH1) = 0x01010101;
+	/* SD CLK */
+	*(volatile u32 *)(CPG_PL2_SDHI_DSEL) = 0x00110011;
+	while (*(volatile u32 *)(CPG_CLK_STATUS) != 0)
+		;
+}
+
+#define PFC_P18				(PFC_BASE + 0x018)
+#define PFC_PM18				(PFC_BASE + 0x130)
+#define PFC_PMC18				(PFC_BASE + 0x218)
+#define PFC_P21				(PFC_BASE + 0x142)
+#define PFC_PM21				(PFC_BASE + 0x128)
+#define PFC_PMC21				(PFC_BASE + 0x221)
+#define PFC_P23				(PFC_BASE + 0x023)
+#define PFC_PM23				(PFC_BASE + 0x146)
+#define PFC_PMC23				(PFC_BASE + 0x223)
+int board_led_init(void)
+{
+	/* RED LED: P8_1 = 0; */
+	*(volatile u32 *)(PFC_PMC18) &= 0xFFFFFFFD;
+	*(volatile u32 *)(PFC_PM18) = (*(volatile u32 *)(PFC_PM18) & 0xFFFFFFF3) | 0x08;
+	*(volatile u32 *)(PFC_P18) = (*(volatile u32 *)(PFC_P18) & 0xFFFFFFFD) | 0x0;
+	/* GREEN LED: P17_2 = 1; */
+	*(volatile u32 *)(PFC_PMC21) &= 0xFFFFFFFB;
+	*(volatile u32 *)(PFC_PM21) = (*(volatile u32 *)(PFC_PM21) & 0xFFFFFFCF) | 0x20;
+	*(volatile u32 *)(PFC_P21) = (*(volatile u32 *)(PFC_P21) & 0xFFFFFFFB) | 0x4;
+	/* BLUE LED: P19_1 = 0; */
+	*(volatile u32 *)(PFC_PMC23) &= 0xFFFFFFFD;
+	*(volatile u32 *)(PFC_PM23) = (*(volatile u32 *)(PFC_PM23) & 0xFFFFFFF3) | 0x08;
+	*(volatile u32 *)(PFC_P23) = (*(volatile u32 *)(PFC_P23) & 0xFFFFFFFD) | 0x0;
+
+    return 0;
+}
+
+int board_early_init_f(void)
+{
+	/* LED's */
+	board_led_init();
+
+	return 0;
+}
+
+#define PFC_P14				(PFC_BASE + 0x014)
+#define PFC_PM14				(PFC_BASE + 0x128)
+#define PFC_PMC14				(PFC_BASE + 0x214)
+int board_eth_init(struct bd_info *bis)
+{
+	/* Ethernet PHY Reset: P4_1 = 1; */
+	*(volatile u32 *)(PFC_PMC14) &= 0xFFFFFFFD;
+	*(volatile u32 *)(PFC_PM14) = (*(volatile u32 *)(PFC_PM14) & 0xFFFFFFF3) | 0x08;
+	*(volatile u32 *)(PFC_P14) = (*(volatile u32 *)(PFC_P14) & 0xFFFFFFFD) | 0x2;
+
+    return 0;
+}
+
+#define PFC_P10				(PFC_BASE + 0x010)
+#define PFC_PM10				(PFC_BASE + 0x120)
+#define PFC_PMC10				(PFC_BASE + 0x210)
+int usbhub_reset(void)
+{
+	/* USB Hub Reset: P0_0 = 1; */
+	*(volatile u32 *)(PFC_PMC10) &= 0xFFFFFFFE;
+	*(volatile u32 *)(PFC_PM10) = (*(volatile u32 *)(PFC_PM10) & 0xFFFFFFFC) | 0x02;
+	*(volatile u32 *)(PFC_P10) = (*(volatile u32 *)(PFC_P10) & 0xFFFFFFFE) | 0x0;
+	mdelay(2);
+	*(volatile u32 *)(PFC_P10) = (*(volatile u32 *)(PFC_P10) & 0xFFFFFFFE) | 0x1;
+
+	return 0;
+}
+
+int board_init(void)
+{
+	/* adress of boot parameters */
+	gd->bd->bi_boot_params = CONFIG_SYS_TEXT_BASE + 0x50000;
+
+	/* reset usbhub */
+	usbhub_reset();
+
+	return 0;
+}
+
+void reset_cpu(void)
+{
+
+}
---
+++ include/configs/rzboard.h
@@ -0,0 +1,120 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2021 Renesas Electronics Corporation
+ */
+
+#ifndef __SMARC_RZV2L_H
+#define __SMARC_RZV2L_H
+
+#include <asm/arch/rmobile.h>
+
+#define CONFIG_REMAKE_ELF
+
+#ifdef CONFIG_SPL
+#define CONFIG_SPL_TARGET	"spl/u-boot-spl.scif"
+#endif
+
+/* boot option */
+
+#define CONFIG_CMDLINE_TAG
+#define CONFIG_SETUP_MEMORY_TAGS
+#define CONFIG_INITRD_TAG
+
+/* Generic Interrupt Controller Definitions */
+/* RZ/V2L use GIC-v3 */
+#define CONFIG_GICV3
+#define GICD_BASE	0x11900000
+#define GICR_BASE	0x11960000
+
+/* console */
+#define CONFIG_SYS_CBSIZE		2048
+#define CONFIG_SYS_BARGSIZE		CONFIG_SYS_CBSIZE
+#define CONFIG_SYS_MAXARGS		64
+#define CONFIG_SYS_BAUDRATE_TABLE	{ 115200, 38400 }
+
+/* PHY needs a longer autoneg timeout */
+#define PHY_ANEG_TIMEOUT		20000
+
+/* MEMORY */
+#define CONFIG_SYS_INIT_SP_ADDR		CONFIG_SYS_TEXT_BASE
+
+/* SDHI clock freq */
+#define CONFIG_SH_SDHI_FREQ		133000000
+
+#define DRAM_RSV_SIZE			0x08000000
+#define CONFIG_SYS_SDRAM_BASE		(0x40000000 + DRAM_RSV_SIZE)
+#define CONFIG_SYS_SDRAM_SIZE		(0x80000000u - DRAM_RSV_SIZE) //total 2GB
+#define CONFIG_SYS_LOAD_ADDR		0x58000000
+#define CONFIG_LOADADDR			CONFIG_SYS_LOAD_ADDR // Default load address for tfpt,bootp...
+#define CONFIG_VERY_BIG_RAM
+#define CONFIG_MAX_MEM_MAPPED		(0x80000000u - DRAM_RSV_SIZE)
+
+#define CONFIG_SYS_MONITOR_BASE		0x00000000
+#define CONFIG_SYS_MONITOR_LEN		(1 * 1024 * 1024)
+#define CONFIG_SYS_MALLOC_LEN		(64 * 1024 * 1024)
+#define CONFIG_SYS_BOOTM_LEN		(64 << 20)
+
+/* The HF/QSPI layout permits up to 1 MiB large bootloader blob */
+#define CONFIG_BOARD_SIZE_LIMIT		1048576
+
+/* support uEnv.txt to pass environment variables to the kernel */
+#ifndef CONFIG_CMD_IMPORTENV
+#define CONFIG_CMD_IMPORTENV
+#endif
+
+#define DEFAULT_MMC_UENV_ARGS \
+	MMC_FDT_OVERLAY_SETTING \
+	"bootenvfile=uEnv.txt\0" \
+	"importbootenv=echo Importing environment from mmc${mmcdev} ...; " \
+		"env import -t ${image_addr} ${filesize}\0" \
+	"loadbootenv=fatload mmc ${mmcdev} ${image_addr} ${bootenvfile}\0" \
+	"envboot=mmc dev ${mmcdev}; " \
+		"if mmc rescan; then " \
+			"echo SD/MMC found on device ${mmcdev};" \
+			"if run loadbootenv; then " \
+				"echo Loaded env from ${bootenvfile};" \
+				"run importbootenv;" \
+			"fi;" \
+		"fi;\0"
+
+#define MMC_BOOT_UENV  "run envboot;"
+#define DEFAULT_MMC_RZV2L_ARGS  DEFAULT_MMC_UENV_ARGS
+
+
+/* ENV setting */
+
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	DEFAULT_MMC_RZV2L_ARGS \
+	"fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0" \
+	"image=Image \0" \
+	"mmcdev="__stringify(CONFIG_SYS_MMC_ENV_DEV)"\0" \
+	"mmcpart=" __stringify(CONFIG_SYS_MMC_ENV_PART) "\0" \
+	"dtb_addr=0x48000000 \0" \
+	"image_addr=0x48080000 \0" \
+	"mmcbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk0p2 \0" \
+	"loadimage=fatload mmc ${mmcdev}:${mmcpart} ${image_addr} ${image}\0" \
+	"loadfdt=echo loading ${fdtfile};fatload mmc ${mmcdev}:${mmcpart} ${dtb_addr} ${fdtfile}\0" \
+	"mmcload=mmc dev ${mmcdev};run loadfdt;run loadimage;run mmcbootargs \0" \
+	"bootimage=run mmcload; booti $image_addr - $dtb_addr \0"
+
+#include "rzboard_overlay.h"
+
+#ifdef AVNET_UENV_FDTO_SUPPORT
+
+#define CONFIG_BOOTCOMMAND      \
+        MMC_BOOT_UENV           \
+        MMC_BOOT_WITH_FDT_OVERLAY \
+		"run bootimage"
+
+#else
+
+#define CONFIG_BOOTCOMMAND	\
+	MMC_BOOT_UENV \
+
+#endif
+
+/* For board */
+/* Ethernet RAVB */
+#define CONFIG_BITBANGMII_MULTI
+
+#endif /* __SMARC_RZV2L_H */
